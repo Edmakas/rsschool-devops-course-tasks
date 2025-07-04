c359adf7aca57ee67bc2af17bd61f1c853e1f7c0
Edmundas <rakauskased@gmail.com>
2025-07-04 15:10:50 +0300

    30

--- Infrastructure/modules/infra/security_groups.tf
+++ Infrastructure/modules/infra/security_groups.tf
@@ -23,6 +23,22 @@ resource "aws_security_group" "public_sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow ICMP (ping) from anywhere"
   }
+  # HTTP (web) from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP from anywhere"
+  }
+  # HTTPS (web) from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS from anywhere"
+  }
   # Allow all outbound traffic
   egress {
     from_port   = 0
