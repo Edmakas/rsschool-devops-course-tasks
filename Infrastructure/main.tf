bb41bc9ffe507e7bc6a68c4553a76eae8e431f2e
Edmundas <rakauskased@gmail.com>
2025-07-05 09:53:42 +0300

    40

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -6,7 +6,7 @@ module "infra" {
   prefix                = var.prefix
   public_key            = var.public_key
   private_key           = var.private_key
-  node_instance_profile = "k3s-node-instance-profile"
+  node_instance_profile = "cif-k3s-node-instance-profile"
   aws_region            = var.aws_region
 }
 
