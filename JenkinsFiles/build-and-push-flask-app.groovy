635b4702277016eb465d1faec7ccc1aaa34d9b1d
Edmundas <rakauskased@gmail.com>
2025-07-16 13:54:59 +0300

    Task_6. JenkinsFile configuration 1

--- JenkinsFiles/build-and-push-flask-app.groovy
+++ JenkinsFiles/build-and-push-flask-app.groovy
@@ -1,5 +1,56 @@
+// pipeline {
+//     agent any
+//     environment {
+//         REGISTRY = 'eckanas/rsschool_flask_app'
+//         IMAGE_TAG = "${env.GIT_COMMIT}"
+//         DOCKER_BUILDKIT = '1'
+//     }
+//     stages {
+//         stage('Checkout') {
+//             steps {
+//                 checkout scm
+//             }
+//         }
+//         stage('Build Docker Image') {
+//             steps {
+//                 script {
+//                     dir('K3S_Manifests/Mod3_Task5/flask_app') {
+//                         sh 'docker build -t $REGISTRY:$IMAGE_TAG .'
+//                     }
+//                 }
+//             }
+//         }
+//         stage('Push Docker Image') {
+//             steps {
+//                 sh 'docker push $REGISTRY:$IMAGE_TAG'
+//             }
+//         }
+//     }
+// } 
+
 pipeline {
-    agent any
+    agent {
+        kubernetes {
+            yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: docker
+    image: docker:24.0.6-cli
+    command:
+    - cat
+    tty: true
+    volumeMounts:
+    - name: dockersock
+      mountPath: /var/run/docker.sock
+  volumes:
+  - name: dockersock
+    hostPath:
+      path: /var/run/docker.sock
+"""
+        }
+    }
     environment {
         REGISTRY = 'eckanas/rsschool_flask_app'
         IMAGE_TAG = "${env.GIT_COMMIT}"
@@ -13,7 +64,7 @@ pipeline {
         }
         stage('Build Docker Image') {
             steps {
-                script {
+                container('docker') {
                     dir('K3S_Manifests/Mod3_Task5/flask_app') {
                         sh 'docker build -t $REGISTRY:$IMAGE_TAG .'
                     }
@@ -22,8 +73,10 @@ pipeline {
         }
         stage('Push Docker Image') {
             steps {
-                sh 'docker push $REGISTRY:$IMAGE_TAG'
+                container('docker') {
+                    sh 'docker push $REGISTRY:$IMAGE_TAG'
+                }
             }
         }
     }
-} 
+}
