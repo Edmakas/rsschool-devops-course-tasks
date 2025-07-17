038302d0f3df78492cf6cec72bf0f14464fc65ac
Edmundas <rakauskased@gmail.com>
2025-07-17 17:38:57 +0300

    Task_6. JenkinsFile configuration 14

--- JenkinsFiles/build-and-push-flask-app.groovy
+++ JenkinsFiles/build-and-push-flask-app.groovy
@@ -74,7 +74,7 @@ spec:
                                 -Dsonar.projectKey=Flask-APP \
                                 -Dsonar.projectBaseDir=/tmp/flask-app  \
                                 -Dsonar.sources=. \
-                                -Dsonar.inclusions=**/*.py \
+                                -Dsonar.inclusions=main.py \
                                 -Dsonar.verbose=true \
                                 -Dsonar.python.version=3"
                             '''
