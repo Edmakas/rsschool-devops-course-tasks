b8d3346fe9b695c2482b92202b8b228f5caf454e
Edmundas <rakauskased@gmail.com>
2025-07-17 17:16:10 +0300

    Task_6. JenkinsFile configuration 14

--- JenkinsFiles/build-and-push-flask-app.groovy
+++ JenkinsFiles/build-and-push-flask-app.groovy
@@ -70,7 +70,7 @@ spec:
                               -e SONAR_TOKEN=$SONAR_TOKEN \
                               -v $(pwd):/usr/src \
                               sonarsource/sonar-scanner-cli \
-                               sh -c "sonar-scanner \
+                               sh -c "ls -al && sonar-scanner \
                                 -Dsonar.projectKey=Flask-APP \
                                 -Dsonar.sources=. \
                                 -Dsonar.projectBaseDir=/usr/src \
