b68dcc1fb9f5e9aea4a794b38b731acebc25565e
Edmundas <rakauskased@gmail.com>
2025-07-18 13:48:48 +0300

    Task_6. JenkinsFile configuration 26

--- JenkinsFiles/build-and-push-flask-app.groovy
+++ JenkinsFiles/build-and-push-flask-app.groovy
@@ -62,18 +62,24 @@ spec:
                             'SONAR_TOKEN=sqp_bb537af4a7bf56e1ec5cac6d855ade31e747cb36'
                         ]) {
                             sh '''
-                            ping 127.0.0.1
-                            apk update && apk add wget unzip openjdk17
-                            wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-5.0.1.3006-linux.zip
-                            unzip sonar-scanner-cli-*.zip
-                            ./sonar-scanner-*/bin/sonar-scanner \
-                                -Dsonar.projectKey=Flask-App \
-                                -Dsonar.sources=. \
-                                -Dsonar.host.url=$SONAR_HOST_URL \
-                                -Dsonar.login=$SONAR_TOKEN
+                            pwd
+                            ls -la
+                            docker run --rm \
+                               -v $(pwd):/tmp/flask-app \
+                               -w /tmp/flask-app \
+                              ubuntu:22.04 \
+                              bash -c "apt update && apt install -y iputils-ping && ping 127.0.0.1"
+                            docker run --rm \
+                              --user $(id -u):$(id -g) \
+                              -e SONAR_HOST_URL=$SONAR_HOST_URL \
+                              -e SONAR_TOKEN=$SONAR_TOKEN \
+                              -v $(pwd):/usr/src \
+                              -w /usr/src \
+                              sonarsource/sonar-scanner-cli \
+                               -Dproject.settings=sonar-project.properties
                             '''
-                            }
-                    }       
+                        }
+                    }
                 }
             }
         }
