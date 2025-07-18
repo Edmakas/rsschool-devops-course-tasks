0acf48f881fd75b7db61e438491a1a2429926eab
Edmundas <rakauskased@gmail.com>
2025-07-18 15:41:53 +0300

    Testing. Commit 1

--- JenkinsFiles/playing.groovy
+++ JenkinsFiles/playing.groovy
@@ -6,8 +6,10 @@ apiVersion: v1
 kind: Pod
 spec:
   containers:
-  - name: docker
-    image: docker:24.0.6-cli
+//    - name: docker
+//      image: docker:24.0.6-cli
+   - name: ubuntu
+    image: ubuntu
     command:
     - cat
     tty: true
