31d2279d0368b317f4327ccd444e0a03226a6634
Edmundas <rakauskased@gmail.com>
2025-07-21 12:07:30 +0300

    Task_6. Added notifications

--- JenkinsFiles/build-and-push-flask-app.groovy
+++ JenkinsFiles/build-and-push-flask-app.groovy
@@ -162,4 +162,20 @@ spec:
             }
         }
     }
+    post {
+        success {
+            emailext (
+                subject: "SUCCESS: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+                body: "Good news! Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' succeeded.\nCheck details at: ${env.BUILD_URL}",
+                to: "${params.NOTIFY_EMAIL ?: env.NOTIFY_EMAIL}"
+            )
+        }
+        failure {
+            emailext (
+                subject: "FAILURE: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+                body: "Unfortunately, job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' failed.\nCheck details at: ${env.BUILD_URL}",
+                to: "${params.NOTIFY_EMAIL ?: env.NOTIFY_EMAIL}"
+            )
+        }
+    }
 }
