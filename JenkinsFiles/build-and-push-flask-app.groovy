708f420baa199fd81cec6e2a5d303692479733fa
Edmundas <rakauskased@gmail.com>
2025-07-22 14:57:33 +0300

    Task_6. Typo

--- JenkinsFiles/build-and-push-flask-app.groovy
+++ JenkinsFiles/build-and-push-flask-app.groovy
@@ -174,7 +174,7 @@ spec:
             emailext (
                 subject: "SUCCESS: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
                 body: "Good news! Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' succeeded.\nCheck details at: ${env.BUILD_URL}",
-                to: "${RECIPIENT_EMAIL}"
+                to: "${RECIPIENT_EMAIL}",
                 from: "${NOTIFY_EMAIL}"
             )
         }
@@ -182,7 +182,7 @@ spec:
             emailext (
                 subject: "FAILURE: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
                 body: "Unfortunately, job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' failed.\nCheck details at: ${env.BUILD_URL}",
-                to: "${RECIPIENT_EMAIL}"
+                to: "${RECIPIENT_EMAIL}",
                 from: "${NOTIFY_EMAIL}"
             )
         }
